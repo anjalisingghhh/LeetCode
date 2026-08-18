d80ce47cf0cebacb81e86ee8910966b3024edab6
anjalisingghhh <as3990315@gmail.com>
2026-08-18 12:39:47 +0200

    Time: 0 ms (100%), Space: 128.4 MB (54.01%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,25 +2,51 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) 
     {
-        // APPROACH - 01
-        vector<int> positive;
-        vector<int> negative;
-
-        for(int i = 0; i < nums.size();i++)
+        // APPROACH - 02
+        vector<int> result(nums.size()); 
+        int positiveIdx = 0;
+        int negativeIdx = 1;
+        for(int i = 0; i < nums.size(); i++)
         {
-            if(nums[i] > 0)
-                positive.push_back(nums[i]);
+            if(nums[i]>0)
+            {
+                result[positiveIdx] = nums[i];
+                positiveIdx += 2;
+            }
             else
-                negative.push_back(nums[i]);
+            {
+                result[negativeIdx] = nums[i];
+                negativeIdx += 2;
+            }
         }
+        return result;
 
-        int p = 0, n = 0, j = 0;
+        
 
-        while (p < positive.size() && n < negative.size())
-        {
-            nums[j++] = positive[p++];
-            nums[j++] = negative[n++];
-        }
-        return nums;
+
+
+//----------------------------------------------------------------------------------------------
+
+
+        // // APPROACH - 01 (BRUTE FORCE)
+        // vector<int> positive;
+        // vector<int> negative;
+
+        // for(int i = 0; i < nums.size();i++)
+        // {
+        //     if(nums[i] > 0)
+        //         positive.push_back(nums[i]);
+        //     else
+        //         negative.push_back(nums[i]);
+        // }
+
+        // int p = 0, n = 0, j = 0;
+
+        // while (p < positive.size() && n < negative.size())
+        // {
+        //     nums[j++] = positive[p++];
+        //     nums[j++] = negative[n++];
+        // }
+        // return nums;
     }
 };

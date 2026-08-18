ce586c208b81327736c2a827018eef3852b1e206
anjalisingghhh <as3990315@gmail.com>
2026-08-18 13:23:22 +0200

    Time: 75 ms (74.63%), Space: 88.9 MB (67.49%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -2,31 +2,66 @@ class Solution {
 public:
     int longestConsecutive(vector<int>& nums)
     {
-        if (nums.empty())
-            return 0;
+        //  APPROACH - 02
+        unordered_set<int> st;
 
-        sort(nums.begin(), nums.end());
-
-        int count = 1;
-        int maxCount = 1;
-
-        for (int i = 1; i < nums.size(); i++)
+        for (int num : nums)
         {
-            if (nums[i] == nums[i - 1] + 1)
-            {
-                count++;
-            }
-            else if (nums[i] == nums[i - 1])
-            {
-                continue;
-            }
-            else
-            {
-                count = 1;
-            }
-
-            maxCount = max(maxCount, count);
+            st.insert(num);
         }
-        return maxCount;
+
+        int maxLen = 0;
+
+        for (int num : st)
+        {
+            // Start counting only if num is the beginning of a consecutive sequence.
+            if (st.find(num - 1) == st.end())
+            {
+                int current = num;
+                int count = 1;
+
+                while (st.find(current + 1) != st.end())
+                {
+                    current++;
+                    count++;
+                }
+
+                maxLen = max(maxLen, count);
+            }  
+        }
+        return maxLen;
+
+
+
+//----------------------------------------------------------------------------------------------
+
+
+        // // APPROACH - 01
+        // if (nums.empty())
+        //     return 0;
+
+        // sort(nums.begin(), nums.end());
+
+        // int count = 1;
+        // int maxCount = 1;
+
+        // for (int i = 1; i < nums.size(); i++)
+        // {
+        //     if (nums[i] == nums[i - 1] + 1)
+        //     {
+        //         count++;
+        //     }
+        //     else if (nums[i] == nums[i - 1])
+        //     {
+        //         continue;
+        //     }
+        //     else
+        //     {
+        //         count = 1;
+        //     }
+
+        //     maxCount = max(maxCount, count);
+        // }
+        // return maxCount;
     }
 };

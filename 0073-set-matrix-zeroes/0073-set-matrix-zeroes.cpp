62cc11533c6b25f62d0f5cd7fc555f5675b95fc7
anjalisingghhh <as3990315@gmail.com>
2026-08-19 09:10:26 +0200

    Time: 0 ms (100%), Space: 21.1 MB (17.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -2,39 +2,80 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) 
     {
-        int rows = matrix.size();
-        int columns = matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-        vector<int> r;
-        vector<int> c;
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
 
-        // Store the row and column of every original zero
-        for (int i = 0; i < rows; i++)
+        // First pass: mark rows and columns that need to be zeroed
+        for (int i = 0; i < m; i++) 
         {
-            for (int j = 0; j < columns; j++)
+            for (int j = 0; j < n; j++) 
             {
-                if (matrix[i][j] == 0)
+                // If element is zero, mark its row and column
+                if (matrix[i][j] == 0) 
                 {
-                    r.push_back(i);
-                    c.push_back(j);
+                    row[i] = 1;
+                    col[j] = 1;
                 }
             }
         }
 
-        // Set corresponding rows and columns to zero
-        for (int i = 0; i < rows; i++)
+        // Second pass: set cells to zero based on markers
+        for (int i = 0; i < m; i++) 
         {
-            for (int j = 0; j < columns; j++)
+            for (int j = 0; j < n; j++) 
             {
-                for (int k = 0; k < r.size(); k++)
+                // If the row or column is marked, set cell to zero
+                if (row[i] == 1 || col[j] == 1) 
                 {
-                    if (i == r[k] || j == c[k])
-                    {
-                        matrix[i][j] = 0;
-                        break;
-                    }
+                    matrix[i][j] = 0;
                 }
             }
         }
+
+
+
+
+//------------------------------------------------------------------------------------------
+
+
+
+        // // APPROACH - 01 (BRUTE FORCE APPROACH)
+        // int rows = matrix.size();
+        // int columns = matrix[0].size();
+
+        // vector<int> r;
+        // vector<int> c;
+
+        // // Store the row and column of every original zero
+        // for (int i = 0; i < rows; i++)
+        // {
+        //     for (int j = 0; j < columns; j++)
+        //     {
+        //         if (matrix[i][j] == 0)
+        //         {
+        //             r.push_back(i);
+        //             c.push_back(j);
+        //         }
+        //     }
+        // }
+
+        // // Set corresponding rows and columns to zero
+        // for (int i = 0; i < rows; i++)
+        // {
+        //     for (int j = 0; j < columns; j++)
+        //     {
+        //         for (int k = 0; k < r.size(); k++)
+        //         {
+        //             if (i == r[k] || j == c[k])
+        //             {
+        //                 matrix[i][j] = 0;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        // }
     }
 };

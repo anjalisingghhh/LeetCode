9e7a9e396c296b7d9310fe2113bbd242303c2577
anjalisingghhh <as3990315@gmail.com>
2026-08-22 17:50:29 +0200

    Time: 0 ms (100%), Space: 23.7 MB (59.98%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,23 +2,87 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
-        // APPROACH - 02
+        // APPROACH - 03
         int n = nums.size(); 
-        vector<int> result; 
-        unordered_map<int, int> mpp;
-        int mini = int(n / 3) + 1;
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = INT_MIN, el2 = INT_MIN;
+
+        for (int i = 0; i < n; i++) 
+        {    
+            if (cnt1 == 0 && el2 != nums[i]) 
+            {
+                cnt1 = 1;
+                el1 = nums[i]; 
+            }
+            else if (cnt2 == 0 && el1 != nums[i]) 
+            {
+                cnt2 = 1;
+                el2 = nums[i]; 
+            } 
+            else if (nums[i] == el1) 
+            {
+                cnt1++;
+            } 
+            else if (nums[i] == el2) 
+            {
+                cnt2++; 
+            } 
+            else 
+            {
+                cnt1--; 
+                cnt2--;
+            }
+        }
+
+        cnt1 = 0, cnt2 = 0; 
         
         for (int i = 0; i < n; i++) 
         {
-            mpp[nums[i]]++;
-            
-            if (mpp[nums[i]] == mini) 
-                result.push_back(nums[i]);
-            
-            if (result.size() == 2) 
-                break;
+            if (nums[i] == el1) 
+            {
+                cnt1++; 
+            }
+            if (nums[i] == el2) 
+            {
+                cnt2++;
+            }
+        }
+        int mini = n / 3 + 1;
+        vector<int> result; 
+
+        if (cnt1 >= mini) 
+        {
+            result.push_back(el1);
+        }
+        if (cnt2 >= mini && el1 != el2) 
+        {
+            result.push_back(el2); 
         }
         return result;
+
+
+
+//----------------------------------------------------------------------------
+
+
+
+        // // APPROACH - 02
+        // int n = nums.size(); 
+        // vector<int> result; 
+        // unordered_map<int, int> mpp;
+        // int mini = int(n / 3) + 1;
+        
+        // for (int i = 0; i < n; i++) 
+        // {
+        //     mpp[nums[i]]++;
+            
+        //     if (mpp[nums[i]] == mini) 
+        //         result.push_back(nums[i]);
+            
+        //     if (result.size() == 2) 
+        //         break;
+        // }
+        // return result;
 
 
 

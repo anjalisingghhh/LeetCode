51ae515d63335ece97e141ec57d52fc78bd334cd
anjalisingghhh <as3990315@gmail.com>
2026-08-22 17:27:30 +0200

    Time: 2315 ms (5.1%), Space: 23.8 MB (59.98%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,50 +2,27 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) 
     {
-        // APPROACH - 02
-        unordered_map<int, int> mpp;
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            mpp[nums[i]]++;
-        }
-
+        // APPROACH - 01
+        int n = nums.size(); 
         vector<int> result;
-
-        for (auto it : mpp)
+        
+        for (int i = 0; i < n; i++) 
         {
-            if (it.second > nums.size() / 3)
+            if (result.size() == 0 || result[0] != nums[i]) 
             {
-                result.push_back(it.first);
+                int cnt = 0;    
+                for (int j = 0; j < n; j++) 
+                {
+                    if (nums[j] == nums[i]) 
+                        cnt++;
+                }
+
+                if (cnt > (n / 3))
+                    result.push_back(nums[i]);
             }
+            if (result.size() == 2) break;
         }
         return result;
-
-//---------------------------------------------------------------------------------------------
-
-        // // APPROACH - 01
-        // int n = nums.size();
-        // vector<int> result;
-
-        // for (int i = 0; i < n; i++)
-        // {
-        //     int count = 1;
-
-        //     for (int j = i + 1; j < n; j++)
-        //     {
-        //         if (nums[i] == nums[j])
-        //             count++;
-        //     }
-
-        //     if (count > n / 3)
-        //     {
-        //         if (find(result.begin(), result.end(), nums[i]) == result.end())
-        //         {
-        //             result.push_back(nums[i]);
-        //         }
-        //     }
-        // }
-        // return result;
     }
 };
 

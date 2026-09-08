be2164f0a7b5bc5c270677af94f78a5d3e710feb
anjalisingghhh <as3990315@gmail.com>
2026-09-08 08:56:08 +0200

    Time: 0 ms (100%), Space: 8.5 MB (84.49%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -2,7 +2,27 @@ class Solution {
 public:
     int countCommas(int n) 
     {
-        return max(0, n-999);
+        // APPROACH - 02 BRUTE FORCE
+        int ans = 0;
+        if(n <= 999)
+            return 0;
+        else
+        {
+            for(int i = 1000; i <= n; i++)
+            {
+                ans += 1;
+            }
+        }
+        return ans;
+
+
+
+
+//----------------------------------------------------------------------------------------------
+
+
+        // // APPROACH - 01 OPTIMISED 
+        // return max(0, n-999);
         
     }
 };

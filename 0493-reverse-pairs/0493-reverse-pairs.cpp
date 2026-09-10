14b6c22607981c7c3b13af7b6a3a007aa02981a3
anjalisingghhh <as3990315@gmail.com>
2026-09-10 18:31:21 +0200

    Time: 536 ms (34.14%), Space: 243.5 MB (33.98%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -0,0 +1,96 @@
+class Solution {
+public:
+
+    // APPROACH - 02 OPTIMSED APPROACH
+    long long merge(vector<int>& nums, int low, int mid, int high) 
+    {
+
+        long long count = 0;
+
+        // Count reverse pairs
+        int j = mid + 1;
+
+        for(int i = low; i <= mid; i++) 
+        {
+
+            while(j <= high &&  (long long)nums[i] > 2LL * nums[j]) 
+            {
+                j++;
+            }
+
+            count += j - (mid + 1);
+        }
+
+        // Merge two sorted halves
+        vector<int> temp;
+
+        int i = low;
+        j = mid + 1;
+
+        while(i <= mid && j <= high) 
+        {
+
+            if(nums[i] <= nums[j])
+                temp.push_back(nums[i++]);
+            else
+                temp.push_back(nums[j++]);
+        }
+
+        while(i <= mid)
+            temp.push_back(nums[i++]);
+
+        while(j <= high)
+            temp.push_back(nums[j++]);
+
+        for(int k = 0; k < temp.size(); k++)
+            nums[low + k] = temp[k];
+
+        return count;
+    }
+
+    long long mergeSort(vector<int>& nums, int low, int high) 
+    {
+
+        if(low >= high)
+            return 0;
+
+        int mid = low + (high - low) / 2;
+
+        long long count = 0;
+
+        count += mergeSort(nums, low, mid);
+        count += mergeSort(nums, mid + 1, high);
+
+        count += merge(nums, low, mid, high);
+
+        return count;
+    }
+
+     int reversePairs(vector<int>& nums) 
+     {
+        return mergeSort(nums, 0, nums.size() - 1);
+        
+
+
+
+
+//----------------------------------------------------------------------------------------------
+
+
+        // // APPROACH - 01 BRUTE FORCE
+        // int count = 0;
+
+        // for(int i = 0; i < nums.size(); i++)
+        // {   
+        //     for(int j = i + 1; j < nums.size(); j++)
+        //     {
+        //         long long multyply = 2;
+        //         long long ele = multyply * nums[j];
+
+        //         if(nums[i] > ele)
+        //             count++;
+        //     }
+        // }
+        // return count;
+    }
+};

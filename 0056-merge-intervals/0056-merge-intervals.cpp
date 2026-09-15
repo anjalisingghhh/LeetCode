740f25e97a31c3117e5e8f3fa063defb38a4763f
anjalisingghhh <as3990315@gmail.com>
2026-09-15 09:27:50 +0200

    Time: 15 ms (6.2%), Space: 24.9 MB (11.65%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,25 +2,47 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) 
     {
-        // APPROACH - 01 
+        // APPROACH - 02
         sort(intervals.begin(), intervals.end());
-        vector<vector<int>> ans;
+        vector<vector<int>> merged;
 
-        int n = intervals.size();
-        for (int i = 0; i < n; ) 
+        for (auto interval : intervals) 
         {
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-
-            int j = i + 1;
-            while (j < n && intervals[j][0] <= end) 
+            if (merged.empty() || merged.back()[1] < interval[0]) 
             {
-                end = max(end, intervals[j][1]);
-                j++;
+                merged.push_back(interval);
+            } 
+            else 
+            {
+                merged.back()[1] = max(merged.back()[1], interval[1]);
             }
-            ans.push_back({start, end});
-            i = j;
         }
-        return ans;
+        return merged;
+
+
+
+//----------------------------------------------------------------------------------------------
+
+
+        // // APPROACH - 01 
+        // sort(intervals.begin(), intervals.end());
+        // vector<vector<int>> ans;
+
+        // int n = intervals.size();
+        // for (int i = 0; i < n; ) 
+        // {
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+
+        //     int j = i + 1;
+        //     while (j < n && intervals[j][0] <= end) 
+        //     {
+        //         end = max(end, intervals[j][1]);
+        //         j++;
+        //     }
+        //     ans.push_back({start, end});
+        //     i = j;
+        // }
+        // return ans;
     }
 };

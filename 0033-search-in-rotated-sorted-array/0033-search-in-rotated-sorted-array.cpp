da4207cae05d591a731eb72705fbd37f3cf274ca
anjalisingghhh <as3990315@gmail.com>
2026-09-16 13:28:33 +0200

    Time: 0 ms (100%), Space: 15.3 MB (37.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,14 +2,48 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) 
     {
-        //APPROACH - 01 BRUTE FORCE
-        for (int i = 0; i < nums.size(); i++) 
+        // APPROACH - 02 OPTIMISED APPROACH - BINARY SEARCH
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while (low <= high) 
         {
-            if (nums[i] == target) 
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid]) 
             {
-                return i;
+                if (nums[low] <= target && target < nums[mid]) 
+                    high = mid - 1;
+                else 
+                    low = mid + 1;
+            }
+
+            else 
+            {
+                if (nums[mid] < target && target <= nums[high]) 
+                    low = mid + 1;
+                else 
+                    high = mid - 1;
             }
         }
-        return -1;        
+        return -1;
+
+
+
+
+//----------------------------------------------------------------------------------------------
+
+
+        // //APPROACH - 01 BRUTE FORCE
+        // for (int i = 0; i < nums.size(); i++) 
+        // {
+        //     if (nums[i] == target) 
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return -1;        
     }
 };

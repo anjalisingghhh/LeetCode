258c0e44f943e79ed1e35532dc588c60be3eb695
anjalisingghhh <as3990315@gmail.com>
2026-09-16 13:38:11 +0200

    Time: 0 ms (100%), Space: 18.1 MB (46.38%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -2,12 +2,53 @@ class Solution {
 public:
     bool search(vector<int>& nums, int target) 
     {
-        // APPROACH - 01 BRUTE FORCE APPROACH
-        for(int i = 0; i < nums.size(); i++)
+        int n = nums.size();
+        int low = 0, high = n - 1;
+
+        while (low <= high) 
         {
-            if(nums[i] == target)
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target)
                 return true;
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) 
+            {
+                low++;
+                high--;
+                continue;
+            }
+
+            if (nums[low] <= nums[mid]) 
+            {
+                if (nums[low] <= target && target < nums[mid]) 
+                    high = mid - 1;
+                else 
+                    low = mid + 1;
+            }
+            
+            else 
+            {
+                if (nums[mid] < target && target <= nums[high]) 
+                    low = mid + 1; 
+                else
+                    high = mid - 1;
+            }
         }
         return false;
+
+
+
+//----------------------------------------------------------------------------------------------
+
+
+
+        // // APPROACH - 01 BRUTE FORCE APPROACH
+        // for(int i = 0; i < nums.size(); i++)
+        // {
+        //     if(nums[i] == target)
+        //         return true;
+        // }
+        // return false;
     }
 };

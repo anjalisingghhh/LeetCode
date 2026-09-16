14c10fbf9826a977276082e2d68b64c3eaf386ed
anjalisingghhh <as3990315@gmail.com>
2026-09-16 12:07:40 +0200

    Time: 0 ms (100%), Space: 13.4 MB (98.51%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -2,20 +2,48 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) 
     {
+        // APPROACH - 02 BINARY SEARCH (LOWER BOUND)
         int left = 0;
         int right = nums.size() - 1;
         int mid;
-        
-        while(left <= right) 
+        int ans = nums.size();
+        while(left <= right)
         {
-            mid = left + (right - left) / 2;
-            if(nums[mid] > target)
+            mid = left + (right - left)/2;
+
+            if(nums[mid] >= target)
+            {
+                ans = mid;
                 right = mid - 1;
-            else if(nums[mid] < target)
-                left = mid + 1;
+            }
             else
-                return mid;
-        }
-        return left;
+                left = mid + 1;
+        } 
+        return ans;
+
+
+
+//----------------------------------------------------------------------------------------------
+
+
+        // //APPROACH - 01
+        // int left = 0;
+        // int right = nums.size() - 1;
+        // int mid;
+        
+        // while(left <= right) 
+        // {
+        //     mid = left + (right - left) / 2;
+
+        //     if(nums[mid] > target)
+        //         right = mid - 1;
+            
+        //     else if(nums[mid] < target)
+        //         left = mid + 1;
+            
+        //     else
+        //         return mid;
+        // }
+        // return left;
     }
 };
